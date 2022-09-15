2862987f430a5d181ae8c143636980357119f64b
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-09-15 15:05:51 +0700

    create: Создал проекты color_descriptor_java и med_lab3_kotlin

---
+++ color_descriptor_java/src/main/java/com/example/color_descriptor_java/ColorDescriptorActivity.java
@@ -0,0 +1,14 @@
+package com.example.color_descriptor_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorDescriptorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_descriptor);
+    }
+}
---
+++ med_lab3_kotlin/src/main/java/com/example/med_lab3_kotlin/Lab3Activity.java
@@ -0,0 +1,14 @@
+package com.example.med_lab3_kotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab3);
+    }
+}
