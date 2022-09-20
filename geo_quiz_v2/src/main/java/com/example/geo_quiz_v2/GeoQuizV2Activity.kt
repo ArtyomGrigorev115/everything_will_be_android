642ab0f08a2cd2126940bb0d27c1c13bd9af8ce3
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-09-20 12:49:31 +0700

    init: создал модуль geo_quiz_v2

---
+++ geo_quiz_v2/src/main/java/com/example/geo_quiz_v2/GeoQuizV2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.geo_quiz_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeoQuizV2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geo_quiz_v2)
+    }
+}
