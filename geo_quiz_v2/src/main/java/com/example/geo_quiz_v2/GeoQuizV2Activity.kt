c13e0674337429c35bb5666ceb0d4f005e688896
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-09-20 13:24:13 +0700

    жизненый цикл активности

--- geo_quiz_v2/src/main/java/com/example/geo_quiz_v2/GeoQuizV2Activity.kt
+++ geo_quiz_v2/src/main/java/com/example/geo_quiz_v2/GeoQuizV2Activity.kt
@@ -2,10 +2,41 @@ package com.example.geo_quiz_v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "GeoQuizV2Activity"
 class GeoQuizV2Activity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geo_quiz_v2)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
